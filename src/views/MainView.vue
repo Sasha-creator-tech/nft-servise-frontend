7cbbff9520dfc7970d44dd145731317a889a87f0
sasha-creator-tech <71944607+Sasha-creator-tech@users.noreply.github.com>
2023-08-12 11:43:26 +0300

    init project

---
+++ src/views/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <home-component/>
+  </div>
+</template>
+
+<script>
+import HomeComponent from "@/components/HomeComponent";
+
+export default {
+  name: "MainView",
+  components: {
+    HomeComponent
+  }
+}
+</script>
